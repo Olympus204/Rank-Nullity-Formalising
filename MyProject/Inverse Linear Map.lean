68c0d5b1926545130f8fda1898e1f72713e6a035
Ben Seymour-Smith <Olympus204@gmail.com>
2025-10-31 13:00:09 +0000

    Update Lean project

---
+++ MyProject/Inverse Linear Map.lean
@@ -0,0 +1,34 @@
+import Mathlib.Algebra.Field.Defs
+import Mathlib.Algebra.Module.LinearMap.Defs
+
+variable {K V W : Type*} [Field K]
+  [AddCommGroup V] [Module K V]
+  [AddCommGroup W] [Module K W]
+
+def idLinear : V →ₗ[K] V :=
+{ toFun := id,
+  map_add' := by simp,
+  map_smul' := by simp }
+
+open Function
+
+noncomputable section
+
+-- Inverse of a linear isomorphism is linear.
+def inverse_map (f : V →ₗ[K] V) (h : Bijective f) : V →ₗ[K] V :=
+{
+  toFun := invFun f.toFun,
+  map_add' := by
+    intro w₁ w₂
+    let p := f.toFun
+    let q := invFun f.toFun
+    let v₁ := q w₁
+    let v₂ := q w₂
+    have h₁ : v₁ + v₂ = q (w₁ + w₂) := by
+      have h₃ : p (v₁ + v₂) = p (v₁) + p (v₂) := by
+        exact f.map_add' v₁ v₂
+      sorry
+    sorry
+    ,
+  map_smul' := sorry
+}
--- MyProject/Linear Map proof.lean
+++ MyProject/Linear Map proof.lean
@@ -1,1 +1,36 @@
-def hello := "world"
+import Mathlib.Algebra.Field.Defs
+import Mathlib.Algebra.Module.LinearMap.Defs
+
+variable {K V W : Type*} [Field K]
+  [AddCommGroup V] [Module K V]
+  [AddCommGroup W] [Module K W]
+
+def idLinear : V →ₗ[K] V :=
+{ toFun := id,
+  map_add' := by simp,
+  map_smul' := by simp }
+
+variable (f : V → W)
+  (h_add : ∀ x y, f (x + y) = f x + f y)
+  (h_smul : ∀ (a : K) (x : V), f (a • x) = a • f x)
+
+-- Define a linear map by hand:
+def myMap  :
+  V →ₗ[K] W :=
+{ toFun := f,
+  map_add' := h_add,
+  map_smul' := h_smul }
+
+example : V →ₗ[K] W :=
+{ toFun := fun x => (2 : K) • (f x)
+  map_add' := by
+    intro x y
+    calc
+      (2 : K) • f (x + y) = (2:K) • (f x + f y) := by rw[h_add]
+      _= (2:K) • f x + (2:K) • f y := by exact DistribSMul.smul_add 2 (f x) (f y)
+  map_smul' := by
+    intro a x
+    calc
+      (2 : K) • f (a • x) = (2 : K) • (a • f x) := by rw[h_smul]
+     _= a • (2:K) • f x := by exact smul_comm 2 a (f x)
+}
