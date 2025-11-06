0c4d797d21b60c69dc344a1453b1ff7a684c10cf
Ben Seymour-Smith <Olympus204@gmail.com>
2025-11-06 10:17:36 +0000

    Refine inverse_map proof

--- MyProject/Inverse Linear Map.lean
+++ MyProject/Inverse Linear Map.lean
@@ -27,8 +27,33 @@ def inverse_map (f : V →ₗ[K] V) (h : Bijective f) : V →ₗ[K] V :=
     have h₁ : v₁ + v₂ = q (w₁ + w₂) := by
       have h₃ : p (v₁ + v₂) = p (v₁) + p (v₂) := by
         exact f.map_add' v₁ v₂
-      rw [ ] at h₃
-    sorry
+      have w₁' : p (v₁) = w₁ := by
+        exact rightInverse_invFun h.surjective w₁
+      have w₂' : p (v₂) = w₂ := by
+        exact rightInverse_invFun h.surjective w₂
+      rw [w₁', w₂'] at h₃
+      calc
+        v₁ + v₂ = q (p (v₁ + v₂)) := (leftInverse_invFun h.injective (v₁ + v₂)).symm
+        _ = q (w₁ + w₂) := by rw[h₃]
+    change invFun f.toFun (w₁ + w₂) = invFun f.toFun w₁ + invFun f.toFun w₂
+    rw[h₁]
+
+
     ,
-  map_smul' := sorry
+  map_smul' := by
+    intro a w
+    let p := f.toFun
+    let q := invFun f.toFun
+    let v := q w
+    have h₁ : a • v = q (a • w) := by
+      have h₂ : p (a • v) = a • p (v) := by
+        exact f.map_smul' a v
+      have w' : p (v) = w := by
+        exact rightInverse_invFun h.surjective w
+      rw[w'] at h₂
+      calc
+        a • v = q (p (a • v)) := (leftInverse_invFun h.injective (a • v)).symm
+        _ = q (a • w) := by rw[h₂]
+    change invFun f.toFun (a • w) = a • invFun f.toFun w
+    rw[h₁]
 }
